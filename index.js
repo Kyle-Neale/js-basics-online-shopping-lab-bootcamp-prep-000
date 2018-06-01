80ff5eb81d406e4ea95d233017e9508b5328b643
Kyle Neale <kyle.a.neale@gmail.com>
2018-06-01 15:41:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,6 +61,6 @@ function placeOrder(cardNumber) {
   if (!cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   } else {
-    
+    return `Your total cost is $${sum}, which will be charged to the card ${cardNumber}.`
   }
 }
